22d031a1d7f9836544f7321d7cab584528c01ddd
zhangfan <zhangfan@guanghe.tv>
2020-05-06 13:56:51 +0800

    添加pytest部分插件

--- test_case/test_case_02.py
+++ test_case/test_case_02.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 '''
 函数阶级方法
 运行于测试 方法 的始末
@@ -16,15 +17,20 @@ class Test_pytest:
     def teardown(self):
         print("Case执行结束")
 
+    @pytest.mark.last   # 最后一个执行
     def test_01(self):
         assert 1 == 1
 
+    # 跳过用例执行
+    @pytest.mark.skip
     def test_02(self):
         assert 2 == 1
 
     def test_03(self):
         assert 2 == 2
 
+    # 用例执行顺序 pip3 install pytest-ordering
+    @pytest.mark.run(order=1)   # 第一个执行
     def test_04(self):
         assert 3 == 2
 
@@ -37,32 +43,42 @@ class Test_pytest:
 '''
 
 
-class Test_class:
-
-    def setup_class(self):
-        print("Case开始执行")
-
-    def teardown_class(self):
-        print("Case执行结束")
-
-    def test_00(self, x):
-        return x+1
-
-    # 如果期望给单一用例添加 重试 和 等待 出错时间
-    # 使用pytest装饰器
-    @pytest.mark.flaky(reruns=2, reruns_delay=2)
-    def test_01(self):
-        assert self.test_00(2) == 1
-
-    def test_02(self):
-        assert 2 == 1
-
-    def test_03(self):
-        assert 2 == 2
-
-    def test_04(self):
-        assert 3 == 3
+# class Test_class:
+#
+#     def setup_class(self):
+#         print("Case开始执行")
+#
+#     def teardown_class(self):
+#         print("Case执行结束")
+#
+#     def test_00(self, x):
+#         return x+1
+#
+#     # 如果期望给单一用例添加 重试 和 等待 出错时间
+#     # 使用pytest装饰器
+#     @pytest.mark.flaky(reruns=2, reruns_delay=2)
+#     def test_01(self):
+#         assert self.test_00(2) == 1
+#
+#     def test_02(self):
+#         assert 2 == 1
+#
+#     def test_03(self):
+#         assert 2 == 2
+#
+#     def test_04(self):
+#         assert 3 == 3
 
 
 if __name__ == '__main__':
-    pytest.main(["-s", "test_case_02.py"])
+    # pytest.main(["-s", "-v", "test_case_02.py"])
+    '''
+    场景:测试用例1000条，一个用例执行1钟，一个测试人员执行需要1000分 钟。
+    通常我们会用人力成本换取时间成本，加几个人一起执行，时间就会缩短。
+    如果10人一起执行只需要100分钟，这就是一种并行测试，分布式场景。
+    解决:pytest分布式执行插件:pytest-xdist，多个CPU或主机执行
+    前提:用例之间都是独立的，没有先后顺序，随机都能执行，可重复运行不 影响其他用例。
+    安装:pip3 install pytest-xdist
+    多个CPU并行执行用例，直接加-n 3是并行数量:pytest -n 3 • 在多个终端下一起执行
+    '''
+    pytest.main(["-n 4", "test_case_02.py"])

cefb15a391c3cfef9a04c303e5704179a8341f62
zhangfan <zhangfan@guanghe.tv>
2020-05-07 21:38:11 +0800

    跟新pytest插件

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ import time
 if __name__ == '__main__':
     rm_rf()  # 调用删除resuit和html文件夹方法
     pytest.main()
-    # allure_auto()  # 调用自动生成报告方法
+    allure_auto()  # 调用自动生成报告方法
     # web_driver()
 
